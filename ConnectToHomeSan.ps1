ceb4d0675719fbdca56d8fe98b300d44843adbdd
Skoyer <lhokie@vt.edu>
2023-10-16 19:49:20 -0400

    Only need to modify for password

---
+++ ConnectToHomeSan.ps1
@@ -0,0 +1,30 @@
+﻿$driveLetter = "Z:"
+$networkPath = "\\192.168.55.250\homevtshare"
+$userId = "myadmin"
+$password = ""
+
+# Test if drive is accessible
+if (-not (Test-Path $driveLetter)) {
+    Write-Output "Drive $driveLetter is not accessible. Attempting to reconnect..."
+
+    # Attempt to disconnect the drive
+    net use $driveLetter /delete /y
+
+    # Wait for a few seconds
+    Start-Sleep -Seconds 2
+
+    # Attempt to reconnect the drive with credentials
+    net use $driveLetter $networkPath /user:$userId $password
+
+    # Wait for a few seconds
+    Start-Sleep -Seconds 2
+
+    # Test again
+    if (-not (Test-Path $driveLetter)) {
+        Write-Output "Failed to reconnect drive $driveLetter."
+    } else {
+        Write-Output "Drive $driveLetter reconnected successfully."
+    }
+} else {
+    Write-Output "Drive $driveLetter is accessible."
+}
---
+++ connectDrive.bat
@@ -0,0 +1,1 @@
+powershell.exe -ExecutionPolicy Bypass -File C:\scripts\ConnectToHomeSan.ps1
